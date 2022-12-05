8f5e3ce145f2bb4c50d60dec103762ff9a8c4f32
Ziad1011 <96190914+Ziad1011@users.noreply.github.com>
2022-12-05 19:36:07 +0200

    Add files via upload

---
+++ MergeSort.js
@@ -0,0 +1,30 @@
+function merge(left, right) {
+    let sortedArr = []; // the sorted elements will go here
+  
+    while (left.length && right.length) {
+      // insert the smallest element to the sortedArr
+      if (left[0] < right[0]) {
+        sortedArr.push(left.shift());
+      } else {
+        sortedArr.push(right.shift());
+      }
+    }
+  }
+  merge([3, 4, 7], [2, 5]);
+
+function merge(left, right) {
+  let sortedArr = []; // the sorted elements will go here
+
+  while (left.length && right.length) {
+    // insert the smallest element to the sortedArr
+    if (left[0] < right[0]) {
+      sortedArr.push(left.shift());
+    } else {
+      sortedArr.push(right.shift());
+    }
+  }
+
+  console.log(sortedArr); // [2, 3, 4, 5]
+  console.log(left); // [7]
+  console.log(right); // []
+}
